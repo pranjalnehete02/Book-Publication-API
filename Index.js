d992a8fd6ccabfb5ab33d28a7c2c07f61e751841
pranjalnehete02 <85827353+pranjalnehete02@users.noreply.github.com>
2021-06-29 16:02:58 +0530

    Book Management
    -

--- Index.js
+++ Index.js
@@ -41,9 +41,9 @@ Mauli.get("/is/:ISBN", (req,res) => {
         return res.json({error:`No book found for the ISBN of ${req.param.ISBN}`,})
     }
 
-    return res.json({book:getSpecificBook});
+    return res.json({book:getSpecificBook}); 
 });
-
+   
 
 
 
@@ -133,13 +133,190 @@ Mauli.get("/publications", (req,res) => {
 
 
 
+/*
+Route           /book/new
+Description     Add new books
+Access          Public
+Parameters      NONE 
+Method          POST
+*/
+Mauli.post("/book/new", (req, res) => {
+    const { newBook } = req.body;
+
+    database.books.push(newBook);
+
+
+    return res.json({ books: database.books, message: "book was added!!" })
+});
+
+
+
+
+/*
+Route           /author/new
+Description     Add new books
+Access          Public
+Parameters      NONE 
+Method          POST
+*/
+Mauli.post("/author/new", (req, res) => {
+    const { neauthork } = req.body;
+
+    database.authors.push(neauthork);
+
+
+    return res.json({ books: database.authors, message: "author was added!!" })
+});
+
+
+
+/*
+Route           /book/update
+Description     update title of a book
+Access          PUBLIC
+Parameters      isbn
+Method          PUT
+*/
+Mauli.put("/book/update/:isbn", (req, res) => {
+    database.books.forEach((book) => {
+        if (book.ISBN === req.params.isbn) {book.title = req.body.bookTitle;return;}
+    });
+  
+    return res.json({ books: database.books });
+  });
+
+
+
+
+  /*
+Route           /book/author/update
+Description     update/add new author
+Access          PUBLIC
+Parameters      isbn
+Method          PUT
+*/
+Mauli.put("/book/author/update/:isbn", (req, res) => {
+    // update the book database
+    database.books.forEach((book) => {if (book.ISBN === req.params.isbn)  return book.authors.push(req.body.newAuthor);
+    });
+  
+    // update the author database
+    database.authors.forEach((author) => {if (author.id === req.body.newAuthor)  return author.books.push(req.params.isbn);
+    });
+  
+    return res.json({
+      books: database.books,
+      authors: database.authors,
+      message: "New author was added 🚀",
+    });
+  });
 
 
 
 
 
+  /*
+Route           /publication/update/book
+Description     update/add new book to a publication
+Access          PUBLIC
+Parameters      isbn
+Method          PUT
+*/
+Mauli.put("/publication/update/book/:isbn", (req, res) => {
+    // update the publication database
+    database.publications.forEach((publication) => {if (publication.id === req.body.pubId) {return publication.books.push(req.params.isbn);}
+    });
+  
+    // update the book database
+    database.books.forEach((book) => {if (book.ISBN === req.params.isbn) {book.publication = req.body.pubId;
+        return;}
+    });
+  
+    return res.json({
+      books: database.books,
+      publications: database.publications,
+      message: "Successfully updated publication",
+    });
+  });
 
 
+  
+/*
+Route           /book/delete
+Description     delete a book
+Access          PUBLIC
+Parameters      isbn
+Method          DELETE
+*/
+Mauli.delete("/book/delete/:isbn", (req, res) => {
+    const updatedBookDatabase = database.books.filter((book) => book.ISBN !== req.params.isbn);
+    database.books = updatedBookDatabase; return res.json({ books: database.books });
+  });
+
+
+
+
+/*
+Route           /book/delete/author
+Description     delete a author from a book
+Access          PUBLIC
+Parameters      isbn, author id
+Method          DELETE
+*/
+Mauli.delete("/book/delete/author/:isbn/:authorId", (req, res) => {
+    // update the book database
+    database.books.forEach((book) => {if (book.ISBN === req.params.isbn) {const newAuthorList = book.authors.filter((author) => author !== parseInt(req.params.authorId));
+        book.authors = newAuthorList;
+        return;}
+    });
+  
+    // update the author database
+    database.authors.forEach((author) => {
+      if (author.id === parseInt(req.params.authorId)) {const newBooksList = author.books.filter((book) => book !== req.params.isbn);  
+        author.books = newBooksList;
+        return;}
+    });
+  
+    return res.json({
+      message: "Author was deleted😤😤😤",
+      book: database.books,
+      author: database.authors,
+    });
+  });
+
+
+
+
+/*
+Route           /publication/delete/book
+Description     delete a book from publication 
+Access          PUBLIC
+Parameters      isbn, publication id
+Method          DELETE
+*/
+Mauli.delete("/publication/delete/book/:isbn/:pubId", (req, res) => {
+    // update publication database
+    database.publications.forEach((publication) => {if (publication.id === parseInt(req.params.pubId)) {const newBooksList = publication.books.filter((book) => book !== req.params.isbn);
+        publication.books = newBooksList;
+        return;}
+    });
+  
+    // update book database
+    database.books.forEach((book) => {if (book.ISBN === req.params.isbn) {book.publication = 0; 
+        // no publication available
+        return;}
+    });
+  
+    return res.json({
+      books: database.books,
+      publications: database.publications,
+    });
+  });
+
+
+ 
+
+  
 Mauli.listen(3000, () => console.log("Server is running😎😎"));
 
 
