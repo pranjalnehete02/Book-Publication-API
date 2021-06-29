66563ee05f2f787a25960c107c827ba1654a5c42
pranjalnehete02 <85827353+pranjalnehete02@users.noreply.github.com>
2021-06-29 12:50:45 +0530

    Book API
    -

--- Index.js
+++ Index.js
@@ -10,7 +10,6 @@ Mauli.use(express.json());
 
 
 
-
 /*
 Route           Line no 18
 Description     get all books
@@ -37,17 +36,17 @@ Method          GET
 */
 Mauli.get("/is/:ISBN", (req,res) => {
     const getSpecificBook = database.books.filter((book) => book.ISBN === req.param.ISBN);
-});
-//to check arewe getting the right ISBN number or not we USE LEGTH
+    //to check arewe getting the right ISBN number or not we USE LEGTH
     if (getSpecificBook.length === 0 ){
-        return res.json({error:`No book found for the ISBN of ${req.param.ISBN}`,});
+        return res.json({error:`No book found for the ISBN of ${req.param.ISBN}`,})
     }
 
     return res.json({book:getSpecificBook});
+});
 
 
 
-    
+
 /*
 Route           catgery
 Description     get specific books on category
@@ -57,12 +56,30 @@ Method          GET
 */
 Mauli.get("/category/:category",(req,res) => {
     const getSpecificBooks = database.books.filter((book) => book.category.includes(req.params.category));
-});
     if (getSpecificBook.length === 0 ){
         return res.json({error:`No book found for the category of ${req.param.category}`,});
     }
 
     return res.json({books:getSpecificBook});
+});
+    
+
+
+
+
+/*
+Route           /authors
+Description     get all authors
+Access          Public
+Parameters      NONE
+Method          GET
+*/
+Mauli.get('/authors', (req, res) => {
+    return res.json({ authors: database.authors});
+});
+
+
+
 
 
 
@@ -89,7 +106,7 @@ Parameters      isbn
 Method          GET
 */
 Mauli.get("/author/;isbn", (req,res) => {
-    const getSpecificAuthors = database.authors((author) => author.books.inncludes(req.params.ISBN));
+    const getSpecificAuthors = database.authors((author) => author.books.includes(req.params.ISBN));
     if(getSpecificAuthors.length === 0 ){
         return res.json({error: `No authors found for the book ${req.params.ISBN}`,})
     }
@@ -111,6 +128,13 @@ Method          GET
 Mauli.get("/publications", (req,res) => {
     return res.json({publications: database.publications});
 });
+
+
+
+
+
+
+
 
 
 
